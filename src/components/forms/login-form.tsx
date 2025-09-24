292e326480329d2283335210ced1f19122ffd5ce
AloysiussG <aloysiusgsetog@gmail.com>
2025-09-25 00:55:42 +0700

    feat: init heroui, shadcn, login page, dashboard page

---
+++ src/components/forms/login-form.tsx
@@ -0,0 +1,72 @@
+import { cn } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { Button, Input } from "@heroui/react";
+import { Label } from "@/components/ui/label";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2 min-h-[28rem]">
+          <form className="p-6 md:p-8 md:pb-10">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome!</h1>
+                <p className="text-muted-foreground text-balance">
+                  Login to your Dexa Group account
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  variant="bordered"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  variant="bordered"
+                  type="password"
+                  required
+                />
+              </div>
+              <div className="pt-2">
+                <Button
+                  type="submit"
+                  className="w-full bg-dexa-primary text-white"
+                >
+                  Login
+                </Button>
+              </div>
+            </div>
+          </form>
+          <div className="bg-muted relative hidden md:flex">
+            <Image
+              className="dark:invert object-cover"
+              src="/dexa-medica-2.jpg"
+              alt="Banner"
+              width={1920}
+              height={1080}
+              priority
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        2025 Dexa Group. Built by Seto Galih for Dexa Group Full-Stack Developer
+        Skill Test.
+      </div>
+    </div>
+  );
+}
---
+++ src/components/sidebars/app-sidebar.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import * as React from "react";
+import {
+  IconDashboard,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconUsers,
+} from "@tabler/icons-react";
+
+import { NavMain } from "@/components/sidebars/navigations/nav-main";
+import { NavUser } from "@/components/sidebars/navigations/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Attendances",
+      url: "#",
+      icon: IconListDetails,
+    },
+    {
+      title: "Employees",
+      url: "#",
+      icon: IconUsers,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-2"
+            >
+              <div className="">
+                <Image
+                  className="h-16 w-auto"
+                  width={256}
+                  height={64}
+                  src="/logo-red.png"
+                  alt="Dexa Group"
+                />
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
