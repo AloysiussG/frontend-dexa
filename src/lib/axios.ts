10a2a857331af43d2bc4ea1df0069b3f01de9538
AloysiussG <aloysiusgsetog@gmail.com>
2025-09-29 00:05:03 +0700

    chore: choose env or config for endpoints

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,8 +3,10 @@ import Axios from "axios";
 // sementara tidak menggunakan .env supaya mudah &
 // dapat langsung dipakai untuk test
 export const URL = {
-  NEXT_FRONTEND_APP_URL: "http://localhost:3000",
-  NEST_BACKEND_APP_URL: "http://localhost:8000",
+  NEXT_FRONTEND_APP_URL:
+    process.env.NEXT_PUBLIC_FRONTEND_APP_URL || "http://localhost:3000",
+  NEST_BACKEND_APP_URL:
+    process.env.NEXT_PUBLIC_BACKEND_APP_URL || "http://localhost:8000",
 };
 
 const axios = Axios.create({
